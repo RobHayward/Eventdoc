91fe4ace2a33c61159044908254976c0c85cb464
RobHayward <robohay@gmail.com>
2015-09-10 12:22:08 +0100

    Extreem2.R updated The file contains three functions to create the currency zoo object, manipulate the zoo object to be compatible with the eventstudy package and to make a statistical and graphical comparison of the extreme and regular currency returns.

--- R/Extreme2.R
+++ R/Extreme2.R
@@ -33,7 +33,8 @@ EUR <- currencydf("EUR")
 head(EUR)
 # Create zoo object for events----------------------------------------
 # Events must be stored in particular format.
-# Requires currencydf zoo object, wind for size of window and hilo for high or low extreme.
+# Requires currencydf zoo object, wind for size of window and hilo 
+#for high or low extreme.
 eventzoo <- function(currencydf, wind, hilo){
   if(hilo == "hi"){
     ho = 4
@@ -47,61 +48,53 @@ eventslist.FXex$unit <- as.character("r")
 colnames(eventslist.FXex) <- c("when", "unit")
 eventslist.FXex$when <-as.Date(eventslist.FXex$when)
 # Align returns with extreme events-----------------------
-a <- phys2eventtime(FX.z, eventslist.FXex, width = wind)
+a <- phys2eventtime(currencydf, eventslist.FXex, width = wind)
 # Create windows (exw - whole, exa - after, cumsum - cumulative sum)
 FX.exw <- window(a$z.e, start = wind * -1, end = wind)
 FX.exa <- window(a$z.e, start = 0, end = wind)
 FX.exwc <- remap.cumsum(FX.exw, is.pc = TRUE, base = 0)
 FX.exac <- remap.cumsum(FX.exa, is.pc = TRUE, base = 0)
-g <- list(FX.exw = FX.exw, FX.exwc = FX.exwc, FX.exac = FX.exac, hilo = hilo)
-return(g)
-}
-t <- eventzoo(currencydf = EUR, wind = 2, hilo = "low")
-hilo = "low"
-head(currencydf$exl)
-str(t)
-currencydf = EUR
-head(currencydf)
-# window size-----------------------------------------
 dims = seq(-wind, wind, 1)
-dim(eventslist.FXex)
 FX.exwcm <- as.matrix(FX.exwc)
 FX.exacm <- as.matrix(FX.exac)
-dim(FX.exacm)
-hist(FX.exacm[10,], prob = TRUE, breaks = 10, main = "Distribution of extreme")
-lines(density(FX.exacm[10,]), col = "red")
 MRW <- mean(FX.exwcm[(wind*2 + 1),])
 MRA <- mean(FX.exacm[(wind + 1),])
-# Mean return whole
-MRW
-# Mean return after event
-MRA
-# Apply the boostrap to calcualte confidence intervals---------------
-FX.exwc.b <- inference.Ecar(FX.exwc, to.plot = TRUE)
-FX.exwc.b
-FX.exac.b <- inference.Ecar(FX.exac, to.plot = TRUE)
-FX.exac.b
-# create charts for comparison of extreme and regular returns. 
- 
+FX.exwc.b <- inference.Ecar(FX.exwc, to.plot = FALSE)
+FX.exac.b <- inference.Ecar(FX.exac, to.plot = FALSE)
+g <- list(MRW = MRW, MRA = MRA, hilo = hilo, wind = wind, currdf = FX.exacm)
+return(g)
+}
+EUR2hi <- eventzoo(currencydf = EUR, wind = 2, hilo = "hi")
+str(EUR2hi)
+ # create charts for comparison of extreme and regular returns. 
+twochart <- function(currencydf, currdf){ 
 par(mfrow = c(2,1))
-plot(density(100*FX.exacm[2,]), xlim = c(-2.0, 2.0), main = "Extreme Density",
+plot(density(100*currdf$currdf[2,]), xlim = c(-2.0, 2.0), main = "Extreme Density",
      lwd = 2, col = "red")
-abline(v = mean(FX.exacm[2,1]))
-text(x = .25, y = 0.4, labels = "Mean")
+abline(v = mean(currdf$currdf[2,], na.rm = TRUE))
+text(x = mean(currdf$currdf[2, ], na.rm = TRUE) + 0.2, y = 0.2, labels = "Mean")
 # need to sample 
-plot(density(sample(FX.z[-1,3], 113), na.rm = TRUE), xlim = c(-2.0, 2.0), 
+plot(density(sample(currencydf[-1,3], 113), na.rm = TRUE), xlim = c(-2.0, 2.0), 
      main = "Regular Density", lwd = 2, col = "red")
-abline(v = mean(FX.z[-1, 3], na.rm = TRUE))
-text(x = .22, y = 0.4, labels = "Mean")
+abline(v = mean(currencydf[-1, 3], na.rm = TRUE))
+text(x = mean(currencydf[-1,3], na.rm = TRUE) + 0.2, y = 0.4, labels = "Mean")
+}
+twochart(EUR, EUR2hi)
+# Compare next day return for extreme and regular
+# THis requires a currencydf and currdf
+comparedist <- function(currencydf, currdf){
 # Mean and standard deviation of return day after extreme
-me <- round(mean(FX.exacm[1,], na.rm = TRUE),4)
-sde <- round(sd(FX.exacm[11,]), 4)
-tex <- round(mean(FX.exacm[11,])/sd(FX.exacm[11,]),4)
+me <- round(mean(currdf$currdf[2,], na.rm = TRUE),4)
+sde <- round(sd(currdf$currdf[2,]), 4)
+tex <- round(mean(currdf$currdf[2,])/sd(currdf$currdf[2,]),4)
 # Mean and standard deviation of return (full sample)
-mr <- round(mean(FX.z[-1,3], na.rm = TRUE),4)
-sdr <- round(sd(FX.z[-1,3], na.rm = TRUE), 4)
-tr <- round(mean(FX.z[-1,3], na.rm = TRUE)/sd(FX.z[-1,3], na.rm = TRUE),4)
+mr <- round(mean(currencydf[-1, 3], na.rm = TRUE),4)
+sdr <- round(sd(currencydf[-1, 3], na.rm = TRUE), 4)
+tr <- round(mean(currencydf[-1, 3], na.rm = TRUE)/
+              sd(currencydf[-1, 3], na.rm = TRUE),4)
 df <- data.frame(ex = c(me, sde, tex), r = c(mr, sdr, tr))
 rownames(df) <- c("Mean", "SD", "T-stat")
-df
+return(df)  
+}
+comparedist(EUR, EUR2hi)
 
